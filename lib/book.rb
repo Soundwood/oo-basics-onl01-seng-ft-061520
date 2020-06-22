4115687e710c0ce5696af662853e1315ed870258
Ben Hogan <j.benhogan@gmail.com>
2020-06-22 22:44:54 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,5 +11,19 @@ def Book
   def author
     @author
   end
+  def page_count=(num)
+    @page_count = num
+  end
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+  end
 end
 
